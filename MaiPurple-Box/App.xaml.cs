c66cf84e775c052612c1a358dcab48abe06600ab
RemMai <maistor@vip.qq.com>
2022-11-30 17:52:49 +0800

    INIT

---
+++ MaiPurple-Box/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Runtime.Serialization.DataContracts;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MaiPurple_Box
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public void SwitchItemCmd()
+        {
+
+
+
+        }
+
+
+    }
+}
---
+++ MaiPurple-Box/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MaiPurple_Box
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Activated(object sender, EventArgs e)
+        {
+            this.Menu.DataContext = new List<string>
+            {
+                "1","2","3","4"
+            };
+        }
+    }
+}
