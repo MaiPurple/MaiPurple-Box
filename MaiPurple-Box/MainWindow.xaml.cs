4e05ece2a25f0bd2d0628ee62181c8f01039b7a1
楠ちゃん <kusu@kusu.moe>
2022-12-05 18:01:07 +0800

    Ver 0.0.1-A3: Add sth.

--- MaiPurple-Box/MainWindow.xaml.cs
+++ MaiPurple-Box/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Windows.Threading.Tasks;
 
 namespace MaiPurple_Box
 {
