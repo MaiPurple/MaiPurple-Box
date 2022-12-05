7678fcee6c1123aac07df5402ac7221a493188f2
楠ちゃん <kusu@kusu.moe>
2022-12-05 17:45:19 +0800

    Ver 0.0.1-A1: Update Useful Code

--- MaiPurple-Box/MainWindow.xaml.cs
+++ MaiPurple-Box/MainWindow.xaml.cs
@@ -24,12 +24,49 @@ namespace MaiPurple_Box
         public MainWindow()
         {
             InitializeComponent();
+            //timer_Tick();
         }
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
         {
             this.DragMove();
             // 设置此属性可以防止拖动到屏幕边缘，窗体最大化
             this.ResizeMode = ResizeMode.NoResize;
+        }
+         private void timer_Tick()
+        {
+            PerformanceCounter cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            PerformanceCounter ramCounter = new PerformanceCounter("Memory", "Available MBytes");
+            Task.Run(() =>
+            {   
+                while (true)
+                {
+                    ramCounter.NextValue();
+                    cpuCounter.NextValue();
+                    Thread.Sleep(1000);
+                    double total = new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory;
+                    double available = 1024.0 * 1024.0 * ramCounter.NextValue();
+                    var cpuUsage = cpuCounter.NextValue();
+                    string cpuUsageStr = string.Format("{0:f2} %", cpuUsage);
+                    var ramAvailable = ramCounter.NextValue();
+                    //var ramleft = 100.0 * (total - used) / total;
+                    string ramleft = string.Format("{0:f2} %", 100.0 * (total - available) / total);
+                    MEM.Dispatcher.Invoke((Action)(() =>
+                    {
+                        MEM.Text = ramleft;
+                        //CPU.Text = cpuUsageStr;
+                    }));
+                    CPU.Dispatcher.Invoke((Action)(() =>
+                    {
+                        //MEM.Text = ramAvaiableStr;
+                        CPU.Text = cpuUsageStr;
+                    }));
+                }
+            });
+
+
+            //My.Computer.Info.TotalPhysicalMemory
+
+
         }
 
     }
